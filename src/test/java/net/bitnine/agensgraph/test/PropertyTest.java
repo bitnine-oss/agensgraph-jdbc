4d40b1d22e1ee452d044f6feb138106887a6e37b
Kyungtae Lee <ktlee@bitnine.net>
2016-08-26 13:14:33 +0900

    feat: support for getters of json primitive type

--- src/test/java/net/bitnine/agensgraph/test/PropertyTest.java
+++ src/test/java/net/bitnine/agensgraph/test/PropertyTest.java
@@ -1,6 +1,7 @@
 package net.bitnine.agensgraph.test;
 
 import junit.framework.TestCase;
+import net.bitnine.agensgraph.graph.property.JsonType;
 import net.bitnine.agensgraph.graph.property.Jsonb;
 import net.bitnine.agensgraph.graph.Vertex;
 import net.bitnine.agensgraph.graph.property.JsonArray;
@@ -35,6 +36,7 @@ public class PropertyTest extends TestCase {
         st.execute("create (:company '{\"name\":\"bitnine\"}')"
                 + "-[:employee '{\"no\":2}']"
                 + "->(:person '{\"name\":\"ktlee\",\"hobbies\":[\"reading\",\"climbing\"],\"age\":null}')");
+        st.execute("CREATE (:person '{ \"name\": \"Emil\", \"from\": \"Sweden\", \"klout\": 99}')");
     }
 
     private void dropSchema() throws Exception {
@@ -70,6 +72,18 @@ public class PropertyTest extends TestCase {
             Jsonb val = (Jsonb)rs.getObject("hobbies");
             assertEquals("reading", val.ja().getString(0));
         }
+        rs = st.executeQuery("MATCH (ee:person) WHERE (ee).klout = to_jsonb(99::int) "
+                + "RETURN (ee).name, (ee).name::text");
+        assertTrue(rs.next());
+        Object val = rs.getObject(1);
+        assertTrue(val instanceof Jsonb);
+        Jsonb jval = (Jsonb)val;
+        assertEquals(JsonType.STRING, jval.getJsonType());
+        String name = jval.getString();
+        assertEquals("Emil", name);;
+        String qname = (String)rs.getString(2);
+        assertEquals("\"Emil\"", qname);
+        assertFalse(rs.next());
         rs.close();
     }
 }
