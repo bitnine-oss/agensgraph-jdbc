91a196e677696fc10cb78c1d59d97891b18b4b19
Beomyong Lee <bylee@bitnine.net>
2018-01-09 17:32:27 +0900

    docs: Update copyright for 2018

--- src/main/java/net/bitnine/agensgraph/util/Jsonb.java
+++ src/main/java/net/bitnine/agensgraph/util/Jsonb.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2017, Bitnine Inc.
+ * Copyright (c) 2014-2018, Bitnine Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
